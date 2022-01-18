59e782aee2e9f5b6d75367b4c5da9663328380d6
My Name <jiographie@gmail.com>
2022-01-18 12:54:00 +0900

    commit 자주 해

---
+++ practice/js/todo.js
@@ -0,0 +1,44 @@
+const toDoForm = document.getElementById('todo-form');
+const toDoInput = document.querySelector('#todo-form input');
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = 'todos';
+
+const toDos = [];
+
+function saveToDos() {
+    // localStorage.setItem('todos', JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintTodo(newTodo) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.innerText = newTodo;
+    const button = document.createElement('button');
+    button.innerText = "❌"
+    button.addEventListener('click', deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    toDos.push(newTodo);
+    paintTodo(newTodo);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const saveToDos = localStorage.getItem(TODOS_KEY);
+
+of
