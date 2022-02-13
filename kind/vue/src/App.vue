b4429f59ad71a427de289ad7e60c9cbac3e25981
jiographie <jiographie@gmail.com>
2022-02-14 00:23:01 +0900

    vue component 적용

---
+++ kind/vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <GlobalComponent></GlobalComponent>
+    <p>app.vue</p>
+</template>
+
+<script>
+import GlobalComponent from './components/global-component.vue'
+  
+
+  export default {
+    name: 'globalComponent',
+    components: {GlobalComponent}
+  }
+</script>
---
+++ kind/vue/src/components/global-component.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <p>뷰 컴포넌트 입니다.</p>
+</template>
+
+<script>
+  export default {
+    name: 'globalComponent',
+      props: {
+      msg: String
+    }
+  }
+
+</script>
+
+<style scoped>
+div {
+  padding: 20px;
+}
+
+div span {
+  margin: 20px;
+}
+</style>
